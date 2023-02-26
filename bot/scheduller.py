de8ab81940667d0947f288324dffcc1996e40db9
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-02-26 15:14:20 +0500

    Update scheduller.py

--- bot/scheduller.py
+++ bot/scheduller.py
@@ -65,7 +65,7 @@ def clear_sheet():
     ws.delete_rows(2, nb_row)
     wb.save('bot/data/example.xlsx')
     
-sched.add_job(send_email, 'cron', hour='15', minute='5')  
+sched.add_job(send_email, 'cron', hour='15', minute='17')  
 
 
 sched.start()
