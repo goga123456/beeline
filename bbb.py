0c2bc114af64274e3fe6c5e95c2b093d2fd54de1
goga123456 <bukanov1234@mail.ru>
2023-03-07 13:57:13 +0500

    add

--- bbb.py
+++ bbb.py
@@ -20,7 +20,7 @@ from core.settings import FILE_EXCEL
 
 filename = FILE_EXCEL
 
-@repeat(every(2).minutes)
+@repeat(every(4).hours)
 def send_email():
     msg = MIMEMultipart("alternative")
     fromaddr = "bukanov1234@mail.ru"
