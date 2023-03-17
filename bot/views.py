3b3344ab67efadfacea39ee0bdbf2d6146fd7d67
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-03-17 18:13:29 +0500

    Update views.py

--- bot/views.py
+++ bot/views.py
@@ -1624,20 +1624,6 @@ def edu(call):
                         wb.close()
                         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
                         less_18(message)     
-
-                elif(now.year - int(user.year)==18):
-                    if(user.month == '0 4'):
-                        if(int(user.day)>=17):
-                            wb = load_workbook(filename)
-                            ws = wb['Лист1']
-                            ws.append([response_date, user.surname, user.name, user.number, birthday])
-                            #ws.cell(row = ws.max_row, column = 5).font = opx.styles.Font(color='ff0816')
-                            wb.save(filename)
-                            wb.close()
-                            bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
-                            less_18(message)   
-
-                
                                          
                 else:
                     bot.send_message(message.chat.id, '5⃣')
